e277a8ae84d8345a5c6f4cfc923176de241afedd
George <tang19970214@gmail.com>
2023-09-10 08:07:55 +0800

    try change mobile height to 100vh

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,20 +4,10 @@ useHead({
     { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" },
   ]
 })
-
-const checkMobile = () => {
-  const flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Andriod|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
-
-  return !!flag
-}
-const isMobile = ref<Boolean>(false)
-onMounted(() => {
-  isMobile.value = checkMobile()
-})
 </script>
 
 <template>
-  <section :class="[isMobile ? 'h-[calc(100vh-80px)]' : 'h-screen']" class="w-full font-mono">
+  <section class="w-full h-screen font-mono">
     <slot />
   </section>
 </template>
